e0595417460ed620f717deaf0b7c162e272ee20d
Adithya Vellal <avellal14@gmail.com>
2019-03-07 18:12:32 -0500

    added code to remove all original .mat files after image has been copied to .png

--- convert_mat_to_png.py
+++ convert_mat_to_png.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.io import loadmat
 from scipy.misc import imsave
 import os
-
+import shutil
 
 def convert_mat_to_png(img_dir):
     img_list = os.listdir(img_dir)
@@ -13,7 +13,6 @@ def convert_mat_to_png(img_dir):
     for img in img_list:
         current_img = os.path.join(img_dir, img)
 
-        print new_png_dir, img
         if not os.path.exists(os.path.join(new_png_dir, img)):
             os.mkdir(os.path.join(new_png_dir, img))
 
@@ -28,13 +27,18 @@ def convert_mat_to_png(img_dir):
 
             img_array = loadmat(os.path.join(img_dir, img, sub_img))['dcmat']
 
+            if(len(os.listdir(new_sub_img_dir)) == img_array.shape[2]):
+                print("skipping because all images have been saved already: " + new_sub_img_dir)
+                os.remove(os.path.join(img_dir, img, sub_img))
+                continue
+
             for i in range (img_array.shape[2]):
                     imsave(os.path.join(new_sub_img_dir, 'slice_' + str(i) + '.png'), img_array[:,:,i])
 
-            print("saved img: " + os.path.join(new_sub_img_dir) + " " + str(img_array.shape[2]))
 
+            print("saved img: " + os.path.join(new_sub_img_dir) + " " + str(img_array.shape[2]))
 
     print "success"
 
-img_path = os.path.join('/root', '.local', 'share', 'Cryptomator', 'mnt', 'VGsIwZZoT9t0_0', 'Train_Subtype', 'Images')
+img_path = os.path.join('/root', '.local', 'share', 'Cryptomator', 'mnt', 'RBcKw0nRRJns_0', 'Train_Subtype', 'Images')
 convert_mat_to_png(img_path)
